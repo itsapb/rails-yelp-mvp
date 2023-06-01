1f4ef1e471d07672cf207152aabf8beeb4abebb4
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-06-01 07:56:57 +0100

    Restauarnts listings added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
+  get 'restaurants/new', to: 'restaurants#new', as: :new
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :review
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = {
+  name: 'Dishoom',
+  address: '7 Boundary St, London E2 7JE',
+  phone_number: '07799727043',
+  category: 'chinese'
+}
+pizza_east = {
+  name: 'Pizza East',
+  address: '56A Shoreditch High St, London E1 6PQ',
+  phone_number: '07799727043',
+  category: 'italian'
+}
+
+le_brasseur = {
+  name: 'Le Brasseur',
+  address: 'Rue du Marché aux Herbes 110, 1000 Bruxelles',
+  phone_number: '02 511 55 83',
+  category: 'belgian'
+}
+
+moules_frites_delight = {
+  name: 'Moules Frites Delight',
+  address: 'Rue du Marché aux Herbes 110, 1000 Bruxelles',
+  phone_number: '02 511 55 83',
+  category: 'belgian'
+}
+
+sakura_sushi_bar = {
+  name: 'Sakura Sushi Bar',
+  address: '106-0041 Tokyo-to, Minato-ku, Azabudai, 1 no 9 no 12',
+  phone_number: '03-3589-4412',
+  category: 'japanese'
+}
+
+zen_teppanyaki = {
+  name: 'Zen Teppanyaki',
+  address: '106-0041 Koyoto-to, Minato-ku, Azabudai, 1 no 9 no 12',
+  phone_number: '03-3589-4434',
+  category: 'japanese'
+}
+
+[dishoom, pizza_east, le_brasseur, moules_frites_delight, sakura_sushi_bar, zen_teppanyaki].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
